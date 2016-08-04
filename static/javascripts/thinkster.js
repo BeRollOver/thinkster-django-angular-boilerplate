8a5cf45a2698cc5e0b5325c708dea25370aa95e9
BeRollOver <dmsizikov@yandex.ru>
2016-08-05 02:53:48 +0300

    Post form done

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,27 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.config',
+      'thinkster.authentication'
+    ]);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+  angular
+    .module('thinkster.config', []);
+})();
+
 angular
-  .module('thinkster', []);
+  .module('thinkster')
+  .run(run);
+
+run.$inject = ['$http'];
+
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
+
